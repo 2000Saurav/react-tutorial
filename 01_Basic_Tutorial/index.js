5126a3fb3c0ded09e789b2b58aa42ec7c5501233
2000Saurav <singhsaurav1707@gmail.com>
2025-08-20 03:51:18 +0530

    Added Swiggy App Clone project inside the react tutorial

--- 01_Basic_Tutorial/index.js
+++ 01_Basic_Tutorial/index.js
@@ -32,14 +32,43 @@ import ReactDOM from "react-dom/client";
 
 */
 
-const parent = React.createElement("div", {id: "parent"},[ React.createElement("div", {id:"child1"},[
-    React.createElement("h1", {}, "This is h1 tag from child 1"),
-    React.createElement("h2", {}, "This is h2 tag from child 1")
-]), React.createElement("div", {id:"child2"}, [
-    React.createElement("h1", {}, "This is h1 tag from child 2"),
-    React.createElement("h2", {}, "This is h2 tag from child 2")
-])])
+// const parent = React.createElement("div", {id: "parent"},[ React.createElement("div", {id:"child1"},[
+//     React.createElement("h1", {}, "This is h1 tag from child 1"),
+//     React.createElement("h2", {}, "This is h2 tag from child 1")
+// ]), React.createElement("div", {id:"child2"}, [
+//     React.createElement("h1", {}, "This is h1 tag from child 2"),
+//     React.createElement("h2", {}, "This is h2 tag from child 2")
+// ])])
 
+
+// now we use JSX to create the same react element
+
+const JSXHeading = <h1 id="heading">This is my first heading</h1>
+console.log(JSXHeading);
+
+
+// JSX is a syntax extension for JavaScript that looks similar to XML or HTML. It allows you to write HTML-like code within your JavaScript files, which is then transformed into React elements by a build tool like Babel.
+// JSX is not a string, it is a React element. It is a syntactic sugar for React.createElement. When you write JSX, it gets compiled to React.createElement calls under the hood.
+// JSX is more readable and easier to write than using React.createElement directly, especially for complex
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(JSXHeading);
+
+const title = <h1 id="title">This is a title from JSX</h1>
+console.log(title);
+// const Title = ()=>{
+//     return <h1 id="title">This is a title from a function</h1>
+// }
+const data = 10000;
+const HeadingComponent = ()=>{
+    
+    return (
+    <div className="container">
+    {title}
+    {data}
+    <h2>This is a heading from a component</h2>
+    </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(< HeadingComponent/>);
